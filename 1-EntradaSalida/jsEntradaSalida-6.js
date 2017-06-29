ed452769c96eb2ff7555f05ce71d023851ac6ba8
marcosebertolini <marcosebertolini@gmail.com>
2017-06-29 15:03:48 -0300

    como pasar de string a integar

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+var num1
+var num2
+var result
+num1 = document.getElementById("numeroUno");
+num2= document.getElementById("numeroDos");
+parseInt(num1);
+parseInt(num2);
+result=(num1+num2);
+alert("el resultado es:"+result.value);
 }
 
