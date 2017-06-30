17b5152087cbd2a09f99ba1eb5838ec668cc9813
marcosebertolini <marcosebertolini@gmail.com>
2017-06-30 20:55:52 -0300

    primera unidad terminada!

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe
+    var importP
+    var resultado
+    importe= document.getElementById("importe").value;
+    importeP= parseInt(importe);
+    resultado = importeP -(importe*0.25)
+    document.getElementById("resultado").value= resultado
 }
