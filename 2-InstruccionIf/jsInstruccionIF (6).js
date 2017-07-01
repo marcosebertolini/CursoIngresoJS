158998ebf1f701651e977fd9d9a0f598f8791a3c
marcosebertolini <marcosebertolini@gmail.com>
2017-06-30 22:10:15 -0300

    desde el 7 hasta el 9 si es posible todosss

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
+edad = document.getElementById("edad").value
+if(edad>17)
+{
+    alert("es mayor");
 
-
-
+}
+else
+{
+    if(edad>=13)
+    {
+        alert("es adolecente");
+    }
+    else
+    {
+       alert("es un niño");
+    }
+}
 
 }//FIN DE LA FUNCIÓN
