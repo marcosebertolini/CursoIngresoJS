3cfafa1c74d79fcb8b0c67b4ecbc841cbea9c394
marcosebertolini <marcosebertolini@gmail.com>
2017-07-01 05:11:16 -0300

    desde el 9 uni2 inclusive!

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
-
+	var edad
+    var estado
+    edad= document.getElementById("edad").value;
+    estado = document.getElementById("estadoCivil").value;
+    if(edad<18 && estado!="Soltero")
+    {
+        alert("es muy joven para no ser soltero!");
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+var num1
+num1 = (Math.random()*10);
+document.getElementById("FormIngreso").value= num1
+
 
 }//FIN DE LA FUNCIÓN
