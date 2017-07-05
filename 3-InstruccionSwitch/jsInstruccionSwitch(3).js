6f82812b1c1b363559e84729d157467aab243390
marcosebertolini <marcosebertolini@gmail.com>
2017-07-04 22:02:50 -0300

    terminado el switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -2,8 +2,15 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+    case "Febrero":
+    alert("Este mes tiene 29 dias")
+    break;
+    default:
+    alert("ESTE MES TIENE 30 DIAS O MAS");
 
-//alert (mesDelAño);
+}
 	
 	
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -3,7 +3,24 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
+switch(laHora)
+{
+    case "7":
+    alert("es de mañana")
+    break;
+    case "8":
+    alert("es de mañana")
+    break;
+    case "9":
+    alert("es de mañana")
+    break;
+    case "10":
+    alert("es de mañana")
+    break;
+    case "11":
+    alert("es de mañana")
+    break;
+}
 	
 	
 
