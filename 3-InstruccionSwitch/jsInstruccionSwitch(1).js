b4cf9d4566ca5f6e9eb63a39ac563484b9e2cc1f
marcosebertolini <marcosebertolini@gmail.com>
2017-07-04 21:33:35 -0300

    terminado hasta el 2 del switch;

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,7 +2,21 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+switch(mesDelAño)
+{
+    case "Enero":
+    alert("QUE INICIES MUY BIEN ESTE AÑO")
+    break;
+    case "Marzo":
+    alert("A CLASEEES!!!")
+    break;
+    case "Julio": 
+    alert("se vienen las vacaciones!!!")
+    break;
+    case "Diciembre":
+    alert("FELICES FIESTAS!")
+    break;
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,42 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Enero":
+    alert("falta para el invierno")
+    break;
+    case "Febrero":
+    alert("falta para el invierno")
+    break;
+    case "Marzo":
+    alert("falta para el inviero")
+    break;
+    case "Abril":
+    alert("falta para el invierno")
+    break;
+    case "Mayo":
+    alert("falta para el invierno")
+    break;
+    case "Junio":
+    alert("falta para el invierno")
+    break;
+    case "Julio":
+    alert("ABRIGATE NO VES QUE ES INVIERNO")
+    break;
+    case "Agosto":
+    alert("ABRIGATE NO VES QUE ES INVIERNO")
+    break;
+    default:
+    alert("YA PASO EL INVIERNO HACE CALOR AHORA, PONETE LA BIKINI");
+
+
+
+
+
+
+
+}
 
 
 
