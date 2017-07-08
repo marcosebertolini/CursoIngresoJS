87c801227d8dbb7541df7c691012c9af74cb4261
marcosebertolini <marcosebertolini@gmail.com>
2017-07-07 21:54:52 -0300

    3 de while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,11 @@ function Mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
+while(clave!="utn750")
+{
+     alert("PASWORD INVALIDA");
+     clave = prompt("VUELVA A INGRESAR");
+}
 
 
 }//FIN DE LA FUNCIÓN
