6d4789e7bf559b9bbccbd675f07c55fd63f7b619
marcosebertolini <marcosebertolini@gmail.com>
2017-07-11 22:11:52 -0300

    en 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,14 +3,50 @@ function Mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	var promepos;
+	var promeneg;
+	var respuesta=true;
+    var acuneg=0;
+	var acupos=0;
+	var contpos=0;
+	var contneg=0;
+	var contcero=0;
+	var contpares=0;
+	var diferencia;
+	while(respuesta==true)
 	{
-		
-	
+		num= prompt("Ingresar numero...");
+		num = parseInt(num);
+		while(isNaN(num))
+		{
+			num=prompt("Ingrese un numero!")
+		}
+	if(num<0)
+	{
+		acuneg+=num;
+		contneg++;
 	}
+	if(num>0)
+	{
+		acupos+=num;
+		contpos++;
+	}
+	if(num==0)
+	{
+		contcero++;
+	}
+	if(num%2==0)
+	{
+		contpares++;
+	}
+    respuesta=confirm("desea seguir???")
+	}//endwhile
+	promepos=contpos/acupos;
+    promeneg= contneg/acuneg;
+	diferencia= contpos-contneg;
+	document.write(diferencia+promepos+promeneg);
+
+    
 
 
 
