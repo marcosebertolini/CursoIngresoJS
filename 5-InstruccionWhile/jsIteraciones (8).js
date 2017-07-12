40a4f032451cc9bdd77875d82b9ed8be6252a1bc
marcosebertolini <marcosebertolini@gmail.com>
2017-07-11 21:18:31 -0300

    hasta el 9 de while

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,28 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var num;
+	var respuesta=true;
+while(respuesta==true)
+{
+	contador++;
+	num=prompt("Ingresar Numero");
+	num=parseInt(num);
+    if(num>=0)
+	{
+		positivo+=num;
+	}
+    else
+	{
+		negativo*=num;
+	}
 
+
+
+
+
+	respuesta=confirm();
+}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,15 +2,37 @@ function Mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
-
-	while(respuesta!='no')
+	var max;
+	var min;
+	var bandera=true;
+	var respuesta=true;
+    var num;
+	while(respuesta==true)
 	{
-		
-	
+		num=prompt("Ingrese numeros...");
+	    num = parseInt(num);
+        if(bandera)
+		{
+			max=num;
+			min=num;
+			bandera=false;
+		}
+		else
+		{
+			if(num>max)
+			{
+				max=num;
+			}
+			if(num<min)
+			{
+				min=num;
+			}
+		}
+		respuesta=confirm("Quiere seguir?")
 	}
+
+document.getElementById("maximo").value=max;
+document.getElementById("minimo").value=min;
 
 
 
